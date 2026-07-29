ce247717479402591283928abf9310f5af54e410
Andres <feliandres412@gmail.com>
2026-07-29 07:32:43 -0500

    arreglos

---
+++ codigo javascript/index.js
@@ -0,0 +1,23 @@
+
+const modo =document.getElementById("check")
+
+    modo.addEventListener("change",() => {
+    if (modo.checked) {
+        window.location.href = "index._regalos.html";
+        
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
